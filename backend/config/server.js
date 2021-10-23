326a26d6bad971faf176b6d8f2a6ab0b1225ea05
Kamil <sikorakamil26@gmail.com>
2021-10-23 14:51:35 +0200

    updated strapi

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7688da4178c66ed3872e2d1befd8d067'),
+    },
+  },
+});
